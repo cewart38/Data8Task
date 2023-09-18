07f831a0b1cf1bc71d1a0fdad79e2f6611f4eeb4
cewart38 <craigewart548@gmail.com>
2023-09-18 10:51:43 +0100

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddHttpClient("Data8Api", client =>
+{
+    client.BaseAddress = new Uri("https://api.data-8.co.uk");
+    client.DefaultRequestHeaders.Add("Authorization", "Bearer NHWE-QAUR-DUI4-S3S6");
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
